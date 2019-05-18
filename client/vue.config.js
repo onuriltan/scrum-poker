7f57bfbe37bfc38bc75fb24c279437dbc1878047
onuriltan <onur.iltan@gmail.com>
2019-05-18 13:28:26 +0300

    add vue-router and vuex to client app

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../server/dist'), // build client app inside server/dist folder
+    devServer: {
+        port: 3000
+    },
+    outputDir: path.resolve(__dirname, '../server/dist'), // build client app inside server/dist folder
 }
